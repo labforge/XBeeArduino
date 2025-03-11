d723535a602a2dfcdcb756a07aa0ffa9cf79c762
Thomas Reidemeister <thomas@labforge.ca>
2025-03-11 18:39:02 -0400

    Disable debugging output

--- src/config.h
+++ src/config.h
@@ -43,14 +43,14 @@ extern "C"
 #define UART_READ_TIMEOUT_MS 100
 #define UART_WRITE_TIMEOUT_MS 10
 
-#define API_FRAME_DEBUG_PRINT_ENABLED 1
+#define API_FRAME_DEBUG_PRINT_ENABLED 0
 #if API_FRAME_DEBUG_PRINT_ENABLED
 #define APIFrameDebugPrint(...) portDebugPrintf(__VA_ARGS__)
 #else
 #define APIFrameDebugPrint(...)
 #endif
 
-#define XBEE_DEBUG_PRINT_ENABLED 1
+#define XBEE_DEBUG_PRINT_ENABLED 0
 #if XBEE_DEBUG_PRINT_ENABLED
 #define XBEEDebugPrint(...) portDebugPrintf(__VA_ARGS__)
 #else
